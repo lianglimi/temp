1504784be55aa26dfea3daa89fefa008a8478e08
丘佳佳[layla] <1406489435@qq.com>
2021-08-18 17:05:26 +0800

    first commit

---
+++ src/components/form.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      姓名：<input  type="text"/>
+      <br/><br/>
+      号码：<textarea type="test"></textarea>
+    </div>
+</template>
+
+<script>
+export default {
+name: "table",
+  data(){
+     return {
+       name:'',
+       phone:''
+     }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
